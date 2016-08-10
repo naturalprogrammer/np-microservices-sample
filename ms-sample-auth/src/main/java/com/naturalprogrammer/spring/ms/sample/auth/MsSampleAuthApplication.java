cd5379b39b66a81d377622082b587319115efbb1
Sanjay <skpatel20@gmail.com>
2016-08-10 14:05:17 +0530

    Added auth server

---
+++ ms-sample-auth/src/main/java/com/naturalprogrammer/spring/ms/sample/auth/MsSampleAuthApplication.java
@@ -0,0 +1,12 @@
+package com.naturalprogrammer.spring.ms.sample.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSampleAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSampleAuthApplication.class, args);
+	}
+}
